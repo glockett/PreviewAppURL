faf81beb1fbd44c00dddb8a6c80ff289ff0e03ab
Gwyn Lockett <gwyn.lockett@theguardian.com>
2016-02-01 16:03:13 +0000

    Amended email function

--- src/main/java/PreviewAppURL.java
+++ src/main/java/PreviewAppURL.java
@@ -1,7 +1,4 @@
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
+import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import java.util.Properties;
@@ -10,18 +7,20 @@ import java.util.Scanner;
 
 public class PreviewAppURL {
 
-    private static String ComposerPreview_URL;
-    private static String PreviewApp_URL;
-    private static final String Environment_URL = "http://viewer.code.dev-gutools.co.uk/preview/";
+    private final String USERNAME = "gwyn.lockett@gmail.com";
+    private final String PWD = "password";
+    private static String PREVIEW_URL;
+    private static final String ENVIRONMENT_URL = "http://viewer.code.dev-gutools.co.uk/preview/";
     private static final String MAPI_URL = "x-gu://preview.mobile-apps.guardianapis.com/items";
-    private static final String SenderAddress = "gumobtest@gmail.com";
-    private static final String Host = "localhost";
+    private static final String SENDER = "gumobtest@gmail.com";
+    private static final String HOST = "localhost";
 
     public static void main(String[] args) {
 
         getAppPreviewURL();
 
         Scanner user_input = new Scanner(System.in);
+
         System.out.print("Please enter the email Address you wish to send the link too:\n");
         String emailAddress = user_input.next();
 
@@ -34,42 +33,54 @@ public class PreviewAppURL {
 
         System.out.print("Please enter the Composer Preview URL:\n");
 
-        ComposerPreview_URL = user_input.next();
-        //Make the correct ComposerURL
-        String s = ComposerPreview_URL.replace(Environment_URL, "");
+        String composerPreview_URL = user_input.next();
 
-        //System.out.println(s);
+        //Create the PREVIEW_URL
+        String s = composerPreview_URL.replace(ENVIRONMENT_URL, "");
 
-        PreviewApp_URL = MAPI_URL + s;
+        System.out.println(s);
 
-        //System.out.println(PreviewApp_URL);
+        PREVIEW_URL = MAPI_URL + s;
+
+        System.out.println(PREVIEW_URL);
     }
 
     public static void sendEmail(String emailAddress) {
 
         String to = emailAddress;
-        String from = "gumobtest@gmail.com";
-        String host = "local";
-        //Get the session object
-        Properties properties = System.getProperties();
-        properties.setProperty("mail.smtp.host", host);
-        Session session = Session.getDefaultInstance(properties);
+        String from = SENDER;
+        String host = HOST;
 
-        //compose the message
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(USERNAME, PWD);
+                    }
+                });
+
         try {
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(from));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-            message.setSubject("Mobile App Preview Link");
-            message.setText("Open the link in the app: " + PreviewApp_URL);
 
-            // Send message
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(emailAddress));
+            message.setSubject("Mobile App - Preview Link");
+            message.setText("Open the link in the app: \n" + PREVIEW_URL);
+
             Transport.send(message);
+
             System.out.println("message sent successfully....");
 
-        } catch (MessagingException mex) {
-            mex.printStackTrace();
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
         }
+
     }
 }
 
