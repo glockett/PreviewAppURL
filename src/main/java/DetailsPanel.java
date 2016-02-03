d67ec2ef13b227568d5318640d3dcb2c3872f5aa
Gwyn Lockett <gwyn.lockett@theguardian.com>
2016-02-03 10:11:23 +0000

    Added GUI

--- src/main/java/DetailsPanel.java
+++ src/main/java/DetailsPanel.java
@@ -2,23 +2,76 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DetailsPanel extends JPanel {
 
+    public static boolean validateEmail(String email) {
+        boolean status = false;
+        String EMAIL_PATTERN =
+                "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-­]+)*@"
+                        + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\­.[A-Za-z]{2,})$";
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.matches()) {
+            status = true;
+        } else {
+            status = false;
+        }
+        return status;
+    }
+
     public DetailsPanel() {
         Dimension size = getPreferredSize();
-        size.width = 500;
+        size.width = 600;
         setPreferredSize(size);
 
         setBorder(BorderFactory.createTitledBorder("Details"));
 
-        JLabel lbl1 = new JLabel("Please enter the Composer Preview URL");
-        JLabel lbl2 = new JLabel("Please enter the Email address (send the Preview link):");
+        //Set up my JImages
 
+        //Setup my JLabels
+        JLabel lbl1 = new JLabel("Please enter the Composer Preview URL");
+        final JLabel errorMsg1 = new JLabel("Error1 msg here!");
+        //errorMsg1.setForeground(Color.getColor(Red));
+        JLabel lbl2 = new JLabel("Please enter the Email address (send the Preview link):");
+        final JLabel errorMsg2 = new JLabel("Error2 msg here!");
+        //errorMsg2.setForeground(Color.getColor(Red));
+
+        //Set up my JTextFields
         final JTextField composerURL = new JTextField(10);
         final JTextField emailAddress = new JTextField(10);
 
+        //Set up my JTextFields
         JButton send_btn = new JButton("Send email");
+
+        send_btn.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+
+                try {
+                    if (composerURL.getText() == "") {
+                        errorMsg1.setText("Please enter the composer Preview URL");
+                    } else if (emailAddress.getText() == "") {
+                        errorMsg2.setText("Please enter the composer Preview URL");
+                        boolean status = validateEmail(emailAddress.getText());
+                        if (status) {
+
+                        } else {
+                            errorMsg2.setText("Not Valid Email address");
+                        }
+                    }
+                } catch (Exception e1) {
+                    e1.printStackTrace();
+                }
+
+                PreviewAppURL.getAppPreviewURL(composerURL.getText());
+
+                PreviewAppURL.sendEmail(emailAddress.getText());
+            }
+        });
+
 
         setLayout(new GridBagLayout());
         GridBagConstraints gc = new GridBagConstraints();
@@ -46,6 +99,7 @@ public class DetailsPanel extends JPanel {
         gc.gridy = 1;
         add(emailAddress, gc);
 
+
         //Column3
         gc.weighty = 10;
         gc.anchor = GridBagConstraints.FIRST_LINE_START;
@@ -53,20 +107,14 @@ public class DetailsPanel extends JPanel {
         gc.gridy = 2;
         add(send_btn, gc);
 
+        gc.anchor = GridBagConstraints.NORTH;
+        gc.gridx = 1;
+        gc.gridy = 3;
+        add(errorMsg1, gc);
 
-
-
-
-
-        //Add behaviours
-        send_btn.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-
-                PreviewAppURL.getAppPreviewURL(composerURL.getText());
-                PreviewAppURL.sendEmail(emailAddress.getText());
-            }
-        });
+        gc.gridx = 1;
+        gc.gridy = 4;
+        add(errorMsg2, gc);
 
 
     }
